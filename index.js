1df1556efbf87b64c3b274fc62d2c81b484e8336
Linda Thomson <linducya@gmail.com>
2020-04-29 17:37:59 +0100

    Using For Loops and Arrays

--- index.js
+++ index.js
@@ -15,25 +15,18 @@ const arrayLength = +response;
     // let myArray = Array(arrayLength);
 let myArray = [];
 myArray.length = arrayLength;
-console.log('let myArray = ' + myArray); // shows myArray empty
-console.log('myArray.length = ' + myArray.length); //outputs length
 
-//Loop to push entered numbers to myArray 
+//Loop to enter numbers to myArray 
 for (let i=0; i < arrayLength; i++) {
     console.log('\nPlease enter number ' + (i + 1) + ':');
     const operand = readline.prompt();
     const number = +operand; //turns string into number
     myArray[i] = number;
-    // myArray.push(number); //set myArray.length to zero using push
 }
-
-// let answer = myArray.reduce(function(accumulator, currentValue) { return accumulator + currentValue }, 0)
-// console.log('testing reduce iterative method ' + answer);
 
 //Read the array by index for calculations
 let answer = myArray[0];
 for (let i = 1; i < myArray.length; i++) {
-    // Example using if and else
     if (operator === '+') {
         answer += myArray[i];
     } else if (operator === '-') {
@@ -45,23 +38,6 @@ for (let i = 1; i < myArray.length; i++) {
     } else {
         console.log('\n' + operator + ' is not a valid operator');
     }
-// Example using switch
-// switch (operator) {
-//     case '+':
-//         answer += myArray[i];
-//         break;
-//     case '-':
-//         answer -= myArray[i];
-//         break;
-//     case '*':
-//         answer *= myArray[i];
-//         break;
-//     case '/':
-//         answer /= myArray[i];
-//         break
-//     default:
-//         console.log(operator + ' is not a valid operator');
-// }
 }
 
 console.log('\nThe answer is: ' + answer);
