c2bfcbb23b766eed63aee2fc11a128dc0907295d
Linda Thomson <linducya@gmail.com>
2020-11-29 12:24:07 +0000

    Added a vowel counting mode: creating a vowel object, counting vowels in a string.

--- index.js
+++ index.js
@@ -1,67 +1,104 @@
 const readline = require('readline-sync');
 
+const ARITHMETIC_MODE = '1';
+const VOWEL_COUNTING_MODE = '2';
+
 function printWelcomeMessage() {
-    console.log('\nWelcome to the calulator!');
-    console.log('=========================');
+    console.log(`\nWelcome to the calulator!
+=========================`);
 }
 
-function enterResponse(prompt) {
-    console.log('\n' + prompt);
+function getStringInput(prompt) {
+    console.log(`\n${prompt}`);
     return readline.prompt();
 }
 
-function convertNumber(prompt) {
+function getNumber(prompt) {
     let response;
     do {
-        response = +enterResponse(prompt);
+        response = +getStringInput(prompt);
     }
     while (isNaN(response));
     return response;
 }
 
 function getOperator() {
+    let operator;
     do   {
-        operator = enterResponse('\nPlease enter the operator: ');
+        operator = getStringInput('Please enter the operator:');
     }
-    while (!(operator == '+' || operator == '-' || operator == '*' || operator == '/'));
+    while (!(operator === '+' || operator === '-' || operator === '*' || operator === '/'));
     return operator;
 }
 
-function getNumberArray() {
-    const iterations = convertNumber('\nHow many numbers do you want to ' + operator + ' ?');
+function getNumberArray(OPERATOR) {
+    const ITERATIONS = getNumber(`How many numbers do you want to ${OPERATOR}?`);
     let myArray = [];
-    myArray.length = iterations;
+    myArray.length = ITERATIONS;
     for (let i = 0; i < myArray.length; i++) {  
-        myArray[i] = convertNumber('\nPlease enter number ' + (i + 1) + ':');
+        myArray[i] = getNumber(`Please enter number ${(i + 1)}:`);
     }
-    console.log(myArray);
     return myArray;
 }
 
-function calculateAnswer(myArray, operator) {
-    let answer = myArray[0];
-    for (let i = 1; i < myArray.length; i++) {
-        if (operator === '+') {
-            answer += myArray[i];
-        } else if (operator === '-') {
-            answer -= myArray[i];
-        } else if (operator === '*') {
-            answer *= myArray[i];
-        } else if (operator === '/') {
-            answer /= myArray[i];
+function calculateAnswer(MYARRAY, OPERATOR) {
+    let answer = MYARRAY[0];
+    for (let i = 1; i < MYARRAY.length; i++) {
+        if (OPERATOR === '+') {
+            answer += MYARRAY[i];
+        } else if (OPERATOR === '-') {
+            answer -= MYARRAY[i];
+        } else if (OPERATOR === '*') {
+            answer *= MYARRAY[i];
+        } else if (OPERATOR === '/') {
+            answer /= MYARRAY[i];
         }
     }
     return answer;
 }
 
-function performOneCalculation() {
-    const operator = getOperator();
-    const myArray = getNumberArray();
-    const answer = calculateAnswer(myArray, operator);
-    return console.log('\nThe answer is: ' + answer);
+function getVowelCounts(UPPERCASE_STRING) {
+    let vowels = {A: 0, E: 0, I: 0, O: 0, U: 0};
+    for (const CHARACTER of UPPERCASE_STRING) {
+        if (vowels.hasOwnProperty(CHARACTER)) {
+            vowels[CHARACTER]++;
+        }
+    }   
+    return vowels;
+}
+
+function performOneArithmeticCalculation() {
+    const OPERATOR = getOperator();
+    const MYARRAY = getNumberArray(OPERATOR);
+    const ANSWER = calculateAnswer(MYARRAY, OPERATOR);
+    return console.log(`\nThe answer is: ${ANSWER}`);
+}
+
+function performOneVowelCountingCalculation() {
+    const STRING = getStringInput('Please enter a string:');
+    const UPPERCASE_STRING = STRING.toUpperCase();
+    const VOWEL_COUNTS = getVowelCounts(UPPERCASE_STRING);
+
+    console.log('\nThe vowel counts are:');
+    for (const PROPERTY in VOWEL_COUNTS) {
+        console.log(`     ${PROPERTY}: ${VOWEL_COUNTS[PROPERTY]}`);
+    }
+    return;
+}
+
+function getCalculationMode() {
+    const MODE = getStringInput(`Which calculation mode do you want?
+    ${ARITHMETIC_MODE}) Arithmetic
+    ${VOWEL_COUNTING_MODE}) Vowel counting`);
+    return MODE;
 }
 
 printWelcomeMessage();
 while (true) {
-    performOneCalculation();
+    const CALCULATIONMODE = getCalculationMode();
+    if (CALCULATIONMODE === ARITHMETIC_MODE) { 
+        performOneArithmeticCalculation();
+    } else if (CALCULATIONMODE === VOWEL_COUNTING_MODE) {
+        performOneVowelCountingCalculation();
+    }
 }
